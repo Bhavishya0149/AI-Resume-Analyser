5a0b5e630c3966ec2082946c270cfb03e61bbcea
Bhavishya0149 <bhavishyabhaskar1@gmail.com>
2026-02-27 07:58:23 +0530

    backend v1

---
+++ backend/src/main/java/com/example/multiauth/AiResumeAnalyser.java
@@ -0,0 +1,12 @@
+package com.example.multiauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiResumeAnalyser {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(AiResumeAnalyser.class, args);
+    }
+}
